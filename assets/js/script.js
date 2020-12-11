58113a199a4104dff1800a2cf259df80f393a6de
randominical <aa.agafonova@gmail.com>
2020-12-11 23:11:22 +0300

    Начало стилизации

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,40 @@
-//появление элементов
 let wrapper = document.querySelector('.wrapper');
 
 let pageSlider = new Swiper('.page', {
-    //переназначение стандартных классов
     wrapperClass: "page__wrapper",
     slideClass: "page__screen",
-
-    //вертикальный слайдер
     direction: 'vertical',
-
-    //количество слайдов для показа
     slidePerView: 'auto',
-
-    //включаем параллакс
     parallax: true,
-    
-    //управление клавиатурой
     keyboard: {
-        //включить/выключить
         enabled: true,
-        //включить/выключить только когда слайдер в пределах вьюпорта
         onlyInViewport: true,
-        //включить/выключить управление клавишами pageUp, pageDown
         pageUpDown: true,
     },
-    
-    //управление колесом мыши
     mousewheel: {
-        //чувствительность колеса мыши
         sensitivity: 1,
-        //класс объекта, на котором будет срабатывать прокрутка мышью
-        //eventsTarget: '.image-slider'
     },
-
-    //отключение функционала, если слайдов меньше, чем нужно
     watchOverflow: true,
-
-    //скорость
     speed: 800,
-
-    //обновить свайпер при изменении элементов слайдера
     observer: true,
-
-    //обновить свайпер при изменении родительских элементов слайдера
     observeParents: true,
-
-    //обновить свайпер при изменении дочерних элементов слайдера
     observeSlideChildren: true,
-
-    //навигация
-    //буллеты, текущее положение, прогрессбар
-    pagination: {
-        el: '.page__pagination',
-        type: 'bullets',
-        clickable: true,
-        bulletClass: 'page__bullet',
-        bulletActiveClass: "page__bullet_active",
-    },
-    //скролл
     scrollbar: {
         el: '.page__scroll',
         dragClass: 'page__drag-scroll',
-        //возможность перетаскивать скролл
         draggable: true
     },
-    
-    //отключение автоинициализации (для работы меню)
     init: false,
-
-    //события
     on: {
-        //инициализация
         init: function () {
             menuSlider();
             setScrollType();
             wrapper.classList.add('_loaded');
         },
-        //смена слайда
         slideChange: function () {
             menuSliderRemove();
             menuLinks[pageSlider.realIndex].classList.add('_active');
         },
-        //изменение размера окна
         resize: function () {
             setScrollType();
         }
@@ -112,7 +65,6 @@ function menuSliderRemove() {
     }
 }
 
-//отключение поэкранной прокрутки при переполнении экрана
 function setScrollType() {
     if (wrapper.classList.contains('_free')) {
         wrapper.classList.remove('_free');
@@ -132,5 +84,4 @@ function setScrollType() {
     }
 }
 
-//инициализируем слайдер вручную
 pageSlider.init();
