5d451eb8e90406b1c73af7f5e6430500f01531ba
randominical <aa.agafonova@gmail.com>
2020-12-21 16:08:41 +0300

    Удаление лишнего кода

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Feb 9, 2021 00:00:00").getTime();
+var countDownDate = new Date("Feb 9, 2021 09:00:00").getTime();
 var x = setInterval(function() {
 var now = new Date().getTime();
 var distance = countDownDate - now;
